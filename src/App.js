8ad602201e06f188ac6c3ef86ad4320a9000cdab
EgorovIvan <sportsmenq@mail.ru>
2022-09-22 16:52:26 +0400

    feat: добавил форму для сообщений и чат, используя хуки useState и useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import './scss/style.scss';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [messageList, setMessageList] = useState([]);
+	
+	useEffect(() => {
+		setMessageList([{
+			id: 1,
+			text: "Чтобы установить мир, нужно просто быть вместе, любить друг друга и приносить ближним мир и радость",
+			author: 'Mother Teresa'
+		}]);
+	}, []);
+	
+	return (
+		<div className="app">
+			<header>
+				<div className="container">
+					<h3 className="title">React</h3>
+				</div>
+			</header>
+			<main>
+				<div className="container">
+					<MessageComponent messageList={messageList}/>
+					<FormMessage setMessageList={setMessageList} messageList={messageList}/>
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default App;
+
+const FormMessage = ({setMessageList, messageList}) => {
+	
+	const [text, setText] = useState('');
+	const [author, setAuthor] = useState('');
+	
+	const addMessage = () => {
+		setMessageList([...messageList, {
+			id: messageList.length + 1,
+			text: text,
+			author: author
+		}]);
+	}
+	
+	return (
+		<div className="form">
+			<h4 className="form__title">Message:</h4>
+			<textarea className="form__message" name="message" cols="30" rows="10"
+			          onChange={event => setText(event.target.value)}></textarea>
+			<h4 className="form__title">Author:</h4>
+			<input className="form__author" type="text" placeholder="ввести имя"
+			       onChange={event => setAuthor(event.target.value)}/>
+			<button className="form__send" onClick={addMessage}>Send</button>
+		</div>
+	)
+}
+
+const MessageComponent = ({messageList}) => {
+	return (
+		<div className="wrapper">
+			<div className="chat">
+				{messageList.map(item => [<div className="messages__item" key={item.id}>
+					<p className="">Text: {item.text}</p>
+					<h3>Author: {item.author}</h3>
+				</div>])}
+			</div>
+		</div>
+	)
+}
